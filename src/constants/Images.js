710b79b3f5c34f9862f5f9d15827cc08424350e4
simbarashenicholaschitovhoro <snchitovhoro@gmail.com>
2021-08-12 16:09:48 -0500

    Fixed UX-UI Ussues

---
+++ src/constants/Images.js
@@ -0,0 +1,33 @@
+
+
+const Images=(theme) =>{
+    const image1 = "https://source.unsplash.com/WLUHO9A_xik/600x900"
+    const image2 = "https://source.unsplash.com/WLUHO9A_xik/600x900"
+    const image3 = "https://source.unsplash.com/WLUHO9A_xik/600x900"
+    const image4 = "https://source.unsplash.com/WLUHO9A_xik/600x900"
+
+
+   const randomImages = [
+        {
+            img: image1,
+            title: 'Computers',
+            author: 'team4author',
+        },
+        {
+            img: image2,
+            title: 'Food',
+            author: 'team4author',
+        },
+        {
+            img: image3,
+            title: 'Cars',
+            author: 'team4author',
+        },
+        {
+            img: image4,
+            title: 'Sheos',
+            author: 'author',
+        },
+    ];
+
+}
